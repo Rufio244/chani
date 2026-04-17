ec5df7c57926e6efee31ed3a43acd06ddfc1a9f7
Rufio244 <thanvas9991@gmail.com>
2026-04-17 09:44:03 +0700

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,1 +1,13 @@
+import axios from "axios"
 
+const API = axios.create({
+  baseURL: "http://localhost:3000/api"
+})
+
+export const sendMessage = (message) => {
+  return API.post("/agent", { message })
+}
+
+export const runComputer = (message) => {
+  return API.post("/computer", { message })
+}
