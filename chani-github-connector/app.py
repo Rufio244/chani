0bc44d0e9a8292334644c0c346737ee36222032a
Rufio244 <thanvas9991@gmail.com>
2026-06-25 08:43:12 +0700

    Implement ChaniGitHub integration with system files

--- chani-github-connector/app.py
+++ chani-github-connector/app.py
@@ -1,1 +1,35 @@
+from github_connector import ChaniGitHub
 
+
+chani = ChaniGitHub()
+
+
+system_files = [
+
+{
+"path":"README.md",
+"content":
+"""
+# Chani AI Core
+
+Auto deployed system.
+"""
+},
+
+{
+"path":"core.py",
+"content":
+"""
+class Chani:
+
+    def think(self,data):
+        return "processing "+str(data)
+"""
+}
+
+]
+
+
+result = chani.push_system(system_files)
+
+print(result)
